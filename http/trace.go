4477370768262786151c312656ec02591b7441b1
hopher.hong <565983236@qq.com>
2023-05-24 17:55:14 +0800

    fix trace

--- http/trace.go
+++ http/trace.go
@@ -2,25 +2,25 @@ package http
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/google/uuid"
 )
 
 // Trace 链路ID，每个请求中增加 requestID，方便日志查询，链路追踪
-func Trace(secret string) Middleware {
+func Trace() Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 			requestID := r.Header.Get("X-Request-Id")
 
-			// 验证密钥，防止 X-Request-Id 伪造
-			// 密钥正确时，则采用上游 X-Request-Id
-			if secret == "" || r.Header.Get("X-Request-Secret") != secret || requestID == "" {
+			if requestID == "" {
 				requestID = uuid.NewString()
+				requestID = strings.Replace(requestID, "-", "", -1)
 			}
 
 			r.Header.Set("X-Request-Id", requestID)
-			w.Header().Set("X-Request-ID", requestID)
+			w.Header().Set("X-Request-Id", requestID)
 
 			next.ServeHTTP(w, r)
 		})
